d70ecb4442e14a55c144664465f3d2df14a18932
kding_u4h3gsr <kdingzon@yahoo.com>
2024-12-27 18:15:07 -0500

    first commit

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def in_autotests_we_trust(a, b):
+    if a == b:
+        print('Test passed')
+    else:
+        print('Test failed')
+
+in_autotests_we_trust(10, '10')
+
+in_autotests_we_trust(0, False)
+

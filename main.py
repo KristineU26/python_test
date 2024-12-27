197fafb4f228f58c3d1473a6d1e61fba6cbafdee
KristineU26 <kdingzon@yahoo.com>
2024-12-27 18:16:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ if __name__ == '__main__':
 
 def in_autotests_we_trust(a, b):
     if a == b:
-        print('Test passed')
+        print('PASS')
     else:
-        print('Test failed')
+        print('FAIL')
 
 in_autotests_we_trust(10, '10')
 
